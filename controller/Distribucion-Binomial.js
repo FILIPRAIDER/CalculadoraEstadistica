755a142ce7960975ba477c96e2018c127769f90a
FILIPRAIDER <filipraider123@gmail.com>
2022-10-16 23:51:58 -0500

    inicio de calculadora1

---
+++ controller/Distribucion-Binomial.js
@@ -0,0 +1,9 @@
+const navToggle = document.querySelector(".toggle")
+const navMenu = document.querySelector(".navbar-nav");
+
+navToggle.addEventListener("click", () => {
+
+    navMenu.classList.toggle("nav-menu_visible");
+
+
+})
