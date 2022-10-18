00caae200d4cd52d108c46a682835820a0edc7db
FILIPRAIDER <filipraider123@gmail.com>
2022-10-17 21:34:00 -0500

    cambios en la resolucion para computadores

--- controller/Distribucion-Binomial.js
+++ controller/Distribucion-Binomial.js
@@ -1,9 +1,12 @@
 const navToggle = document.querySelector(".toggle")
 const navMenu = document.querySelector(".navbar-nav");
 
+
 navToggle.addEventListener("click", () => {
 
     navMenu.classList.toggle("nav-menu_visible");
 
 
 })
+
+
