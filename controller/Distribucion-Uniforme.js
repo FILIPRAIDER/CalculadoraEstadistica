ade4b901bb3c848952f0275902cce40d49f0a1eb
FILIPRAIDER <filipraider123@gmail.com>
2022-11-02 18:30:28 -0500

    cambios

---
+++ controller/Distribucion-Uniforme.js
@@ -0,0 +1,10 @@
+const navToggle = document.querySelector(".toggle")
+const navMenu = document.querySelector(".navbar-nav");
+
+
+navToggle.addEventListener("click", () => {
+
+    navMenu.classList.toggle("nav-menu_visible");
+
+
+})
